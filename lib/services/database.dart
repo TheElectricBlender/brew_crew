1de9454d0665d53a009297595cf2a9ea5e39ad50
TheElectricBlender <yodudee97@gmail.com>
2020-03-03 20:14:14 +0800

    1.Added brew list into models from snapshot (iterate through data in doc)

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:brew_crew/models/brew.dart';
 
 // method and properties to be used to interact with database
 class DatabaseService {
-
   final String uid;
+
   DatabaseService({this.uid});
 
   // collection reference
@@ -20,6 +21,19 @@ class DatabaseService {
     });
   }
 
+  // get brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+        name: doc.data['name'] ?? '',
+        strength: doc.data['strength'] ?? 0,
+        sugars: doc.data['sugars'] ?? '0',
+      );
+    }).toList(); // because it returns an iterable
+  }
 
-
+  // get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
 }
