682a4688d4ac5dab5ca535e52081f24537e62425
TheElectricBlender <yodudee97@gmail.com>
2020-03-03 21:10:13 +0800

    1. Implemented settings form using DropdownButtonFormField

---
+++ lib/screens/home/settings_form.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:brew_crew/shared/constants.dart';
+
+// stateful as users key into text which changes state
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+
+  // form values
+  String _currentName;
+  String _currentSugars;
+  int _currentStrength;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Update your brew settings',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            decoration: textInputDecoration,
+            validator: (val) {
+              val.isEmpty ? 'Please enter a name' : null;
+            },
+            onChanged: (val) {
+              setState(() {
+                _currentName = val;
+              });
+            },
+          ),
+          SizedBox(height: 20.0),
+          // dropdown
+          DropdownButtonFormField(
+            decoration: textInputDecoration,
+            value: _currentSugars ?? '0', // value that is shown in the form field
+            items: sugars.map((sugar) {
+              return DropdownMenuItem(
+                value: sugar, // value the user selects 1, 2, 3, 4
+                child: Text('$sugar sugars'),
+              );
+            }).toList(),
+            onChanged: (val) {
+              setState(() {
+                _currentSugars = val;
+              });
+            },
+          ),
+          // slider
+          RaisedButton(
+            color: Colors.pink[400],
+            child: Text(
+              'Update',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () async {
+              // send data to store in firebase
+              print(_currentName);
+              print(_currentSugars);
+              print(_currentStrength);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
